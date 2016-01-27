d3df2532832c0603f116519b48c98093ce8277fc
Ashita Agrawal <agr.ash@gmail.com>
2016-01-27 13:01:15 +0530

    Initial boilerplate code

---
+++ app/src/main/java/code/aroundme/BlankFragment.java
@@ -0,0 +1,22 @@
+package code.aroundme;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BlankFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.activity_blank, container, false);
+        return view;
+    }
+
+}
